7577747523b1c6a79d47b05790b71ed5369d43b3
JayeshHande <94224905+jayeshHande21@users.noreply.github.com>
2022-11-28 12:30:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -251,10 +251,38 @@ btnSort.addEventListener("click", function (e) {
   sorted = !sorted;
 });
 
+
 /////////////////////////////////////////////////
 
-// const max = movements.reduce((acc, curr) => {
-//   if (acc > curr) return acc;
-//   else return curr;
-// }, movements[0]);
-// console.log(max)
+// <?php
+// $firstName = $_POST['firstName'];
+// $lastName = $_POST['lastName'];
+// $motherName = $_POST['motherName'];
+// $fatherName = $_POST['fatherName'];
+// $address = $_POST['address'];
+// $gender  = $_POST['gender'];
+// $DOB = $_POST['DOB'];
+// $pincode = $_POST['pincode'];
+// $course  = $_POST[''];
+
+// $email = $_POST['email'];
+
+// $conn = mysqli_connect('localhost' , 'root','','connectbase');
+// echo "Connection Was sucessfull"
+// if($conn->connect_error){  
+//     die('Connection Failed :' .$conn->connect_error );
+// }else{
+//     $stmt = $conn->prepare("Insert into registration(firstName,lastName,motherName,fatherName,address,gender,DOB,pincode,course,email)
+//     values(?,?,?,?,?,?,?,?,?,?)");
+//     $stmt->bind_param("ssssssiiss" ,$firstName,$lastName,$motherName,$fatherName,$address,$gender,$DOB,$pincode,$course,$email);
+//     $stmt->execute();
+//     echo "Registration Successfully....";
+//     $stmt ->close();
+//     $conn ->close();
+    
+// }
+
+// ?>
+
+
+   
